5e7f5623d8c65ecf6dc436664151cc7520795dee
沈正义 <shenzhengyi@shinezone.com>
2018-02-23 10:21:34 +0800

    注释格式修改

--- lib/backend/json-schema/index.ts
+++ lib/backend/json-schema/index.ts
@@ -2,11 +2,11 @@
  * @Author: shenzhengyi 
  * @Date: 2018-02-01 17:51:24 
  * @Last Modified by: shenzhengyi
- * @Last Modified time: 2018-02-07 17:00:27
+ * @Last Modified time: 2018-02-23 10:20:52
  */
 
 import AST from "../../ast";
-import { OUTTAG, APINode } from "../../astnode";
+import { OUTTAG, APINode, VarNode } from "../../astnode";
 
 export default function (ast: AST, outPath: string, ejsPath: string) {
     let map = ast.getAPIMap(OUTTAG.server);
@@ -17,9 +17,39 @@ export default function (ast: AST, outPath: string, ejsPath: string) {
             list = new Array<APINode>();
             apiMap.set(an.mod, list);
         }
+        list.push(an);
+    }
 
-        
+    for (let [k, list] of apiMap) {
+        let data: any = {};
+        for (let an of list) {
+            {
+                let [prop, red] = getProperties(an.req.args);
+                let reqName = `${an.mod}/${an.name}/req`;
+                data[reqName] = {
+                    comment: an.req.comment,
+                    $id: reqName,
+                    properties: prop,
+                    required: red
+                };
+            }
+
+            {
+                let [prop, red] = getProperties(an.res.args);
+                let resName = `${an.mod}/${an.name}/resp`;
+                data[resName] = {
+                    comment: an.res.comment,
+                    $id: resName,
+                    properties: prop,
+                    required: red
+                }
+            }
+        }
     }
 }
 
 
+function getProperties(args: VarNode[]) {
+
+    return [{}, []];
+}
